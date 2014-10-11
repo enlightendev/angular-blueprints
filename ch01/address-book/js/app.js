6bbd62482ff2e970e80ac97341080726aa486b69
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-10-11 08:17:04 -0400

    ng-hide/show to display input form as needed.

--- ch01/address-book/js/app.js
+++ ch01/address-book/js/app.js
@@ -1,6 +1,5 @@
 function PeopleController($scope){
 
-
     $scope.people=[
         {name:"John Doe", phone: "3452345678", city:"New York"},
         {name:"Sarah Parker", phone: "1236548769", city:"Chicago"},
@@ -8,11 +7,27 @@ function PeopleController($scope){
         {name:"Adam Doe", phone: "9025673152", city:"Las Vegas"}
     ];
 
+    $scope.showForm = function(){
+        $scope.formVisibility = true;
+    };
+
+    $scope.cancelForm = function(){
+
+        $scope.formVisibility = false;
+
+        $scope.newPerson = {};
+    };
+
     $scope.save = function(){
+
         $scope.people.push({
            name: $scope.newPerson.name,
            phone: $scope.newPerson.phone,
            city: $scope.newPerson.city
         });
-    }
+
+        $scope.newPerson = {};
+
+        $scope.formVisibility = false;
+    };
 }
