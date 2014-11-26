11f325829f14bf2360625dbb3cbcd3a6014394f1
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-11-26 09:18:54 -0500

    ch06 - mobile app - refactored business logic out of controller and into service.

--- ch06-mobile/app/scripts/controllers.js
+++ ch06-mobile/app/scripts/controllers.js
@@ -19,18 +19,7 @@ angular.module('app.controllers', [])
 
     $scope.expenses = expenseService.getExpenses();
 
-    $scope.summaryData = [];
-    var categories=categoryList;
-
-    categories.forEach(function(item) {
-        var catTotal = expenseService.getCategoryTotal(item);
-
-        $scope.summaryData.push({
-          category: item,
-          amount: catTotal
-        });
-
-      });
+    $scope.summaryData = expenseService.getSummaryData();
 
   });
 
--- ch06-mobile/app/scripts/services.js
+++ ch06-mobile/app/scripts/services.js
@@ -4,7 +4,7 @@ angular.module('app.services', [])
 
   .value('categoryList', ['Food', 'Fuel', 'Grocery', 'Entertainment'])
 
-  .factory('expenseService', function () {
+  .factory('expenseService', function (categoryList) {
 
     /**
      * Firstly, we are creating a prefix for all data that is being stored as a part of this app.
@@ -15,75 +15,94 @@ angular.module('app.services', [])
      */
     var prefix = 'exp-mgr';
 
-    return {
+    var exports = {};
+
+    /**
+     * this will be called when the Submit button of the form is hit. The saveExpense function takes in the data as an input parameter.
+     * @param data
+     */
+    exports.saveExpense = function (data) {
+      var timeStamp = Math.round(new Date().getTime());
+      var key = prefix + timeStamp;
 
       /**
-       * this will be called when the Submit button of the form is hit. The saveExpense function takes in the data as an input parameter.
-       * @param data
+       * As localStorage can store the values only as strings, we need to convert our JSON data into a string.
        */
-      saveExpense: function (data) {
-        var timeStamp = Math.round(new Date().getTime());
-        var key = prefix + timeStamp;
+      data = JSON.stringify(data);
 
-        /**
-         * As localStorage can store the values only as strings, we need to convert our JSON data into a string.
-         */
-        data = JSON.stringify(data);
-
-        /**
-         * save to local storage
-         */
-        localStorage[key] = data;
-      },
-
-      getExpenses: function () {
-
-        var expenses = [];
-        var prefixLength = prefix.length;
-
-        /**
-         * LocalStorage is that it doesn't have advanced methods to filter data or return a set of matching records.
-         * We need to iterate through each item in localStorage and selectively pick up the ones that match our needs.
-         */
-        Object.keys(localStorage).forEach(function (key) {
-
-          if (key.substring(0, prefixLength) === prefix) {
-            var item = window.localStorage[key];
-
-            /**
-             * JSON.parse to convert our stringified data back into JSON format.
-             */
-            item = JSON.parse(item);
-
-            expenses.push(item);
-          }
-        });
-
-        return expenses;
-      },
-
-      getCategoryTotal: function(category){
-
-        var categoryTotal = 0;
-        var prefixLength = prefix.length;
-
-        Object.keys(localStorage).forEach(function (key) {
-
-          if (key.substring(0, prefixLength) === prefix) {
-            var item = window.localStorage[key];
-
-            /**
-             * JSON.parse to convert our stringified data back into JSON format.
-             */
-            item = JSON.parse(item);
-
-            if (item.category === category) {
-              categoryTotal += parseFloat(item.amount); //need to use parseFloat to convert our numbers from string to a float value before we total them up.
-            }
-          }
-        });
-
-        return categoryTotal;
-      }
+      /**
+       * save to local storage
+       */
+      localStorage[key] = data;
     };
+
+    exports.getExpenses = function () {
+
+      var expenses = [];
+      var prefixLength = prefix.length;
+
+      /**
+       * LocalStorage is that it doesn't have advanced methods to filter data or return a set of matching records.
+       * We need to iterate through each item in localStorage and selectively pick up the ones that match our needs.
+       */
+      Object.keys(localStorage).forEach(function (key) {
+
+        if (key.substring(0, prefixLength) === prefix) {
+          var item = window.localStorage[key];
+
+          /**
+           * JSON.parse to convert our stringified data back into JSON format.
+           */
+          item = JSON.parse(item);
+
+          expenses.push(item);
+        }
+      });
+
+      return expenses;
+    };
+
+    exports.getCategoryTotal = function (category) {
+
+      var categoryTotal = 0;
+      var prefixLength = prefix.length;
+
+      Object.keys(localStorage).forEach(function (key) {
+
+        if (key.substring(0, prefixLength) === prefix) {
+          var item = window.localStorage[key];
+
+          /**
+           * JSON.parse to convert our stringified data back into JSON format.
+           */
+          item = JSON.parse(item);
+
+          if (item.category === category) {
+            categoryTotal += parseFloat(item.amount); //need to use parseFloat to convert our numbers from string to a float value before we total them up.
+          }
+        }
+      });
+
+      return categoryTotal;
+    };
+
+    exports.getSummaryData = function () {
+
+      var summaryData = [];
+      var categories = categoryList;
+
+      categories.forEach(function (item) {
+        var catTotal = exports.getCategoryTotal(item);
+
+        summaryData.push({
+          category: item,
+          amount: catTotal
+        });
+
+      });
+
+      return summaryData;
+    };
+
+    return exports;
   });
