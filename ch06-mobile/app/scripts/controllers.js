433e7b3ec7c7eff459ccd2ecd72881b6bc12da7c
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-11-26 08:48:47 -0500

    ch06 - mobile app - ability to add/view expenses

--- ch06-mobile/app/scripts/controllers.js
+++ ch06-mobile/app/scripts/controllers.js
@@ -1,14 +1,21 @@
-angular.module('app.controllers',[])
-  .controller('HomeCtrl',function(){
+angular.module('app.controllers', [])
+  .controller('HomeCtrl', function () {
 
   })
-  .controller('AddExpenseCtrl',function($scope, categoryList){
+  .controller('AddExpenseCtrl', function ($scope, categoryList, expenseService, $location) {
 
     $scope.categories = categoryList;
 
+    $scope.submit = function () {
+      expenseService.saveExpense($scope.expense);
+      $location.path('/view-summary');
+    }
+
   })
 
-  .controller('ViewSummaryCtrl',function(){
+  .controller('ViewSummaryCtrl', function ($scope, expenseService) {
+
+    $scope.expenses = expenseService.getExpenses();
 
   });
 
--- ch06-mobile/app/scripts/services.js
+++ ch06-mobile/app/scripts/services.js
@@ -1,2 +1,64 @@
-angular.module('app.services',[])
-  .value('categoryList',["Food", "Fuel", "Grocery", "Entertainment"]);
+angular.module('app.services', [])
+
+  .value('categoryList', ["Food", "Fuel", "Grocery", "Entertainment"])
+
+  .factory('expenseService', function () {
+
+    /**
+     * Firstly, we are creating a prefix for all data that is being stored as a part of this app.
+     * Remember that localStorage stores data as simple key-value pairs. These keys are unique to
+     * each domain. To make sure that we are interacting with data belonging to only our app and
+     * to also to make it easier while retrieving the data, we prefix all our keys with the exp-mgr prefix.
+     * @type {string}
+     */
+    var prefix = 'exp-mgr';
+
+    return {
+
+      /**
+       * this will be called when the Submit button of the form is hit. The saveExpense function takes in the data as an input parameter.
+       * @param data
+       */
+      saveExpense: function (data) {
+        var timeStamp = Math.round(new Date().getTime());
+        var key = prefix + timeStamp;
+
+        /**
+         * As localStorage can store the values only as strings, we need to convert our JSON data into a string.
+         */
+        data = JSON.stringify(data);
+
+        /**
+         * save to local storage
+         */
+        localStorage[key] = data;
+      },
+
+      getExpenses: function () {
+
+        var expenses = [];
+        var prefixLength = prefix.length;
+
+        /**
+         * LocalStorage is that it doesn't have advanced methods to filter data or return a set of matching records.
+         * We need to iterate through each item in localStorage and selectively pick up the ones that match our needs.
+         */
+        Object.keys(localStorage).forEach(function (key) {
+
+          if (key.substring(0, prefixLength) == prefix) {
+            var item = window.localStorage[key];
+
+            /**
+             * JSON.parse to convert our stringified data back into JSON format.
+             */
+            item = JSON.parse(item);
+
+            expenses.push(item);
+          }
+        });
+
+        return expenses;
+      }
+    }
+  })
+;
