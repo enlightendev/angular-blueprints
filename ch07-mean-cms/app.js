9b35b16e67f81b9ef3b07f89e782e2ab3166801f
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-11-27 07:44:39 -0500

    ch07 - mean stack cms - db connection and schema

--- ch07-mean-cms/app.js
+++ ch07-mean-cms/app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/angcms');
+var db = mongoose.connecetion;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
